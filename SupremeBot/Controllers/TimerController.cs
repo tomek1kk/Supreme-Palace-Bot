a789d0c882fedcec6a8a43109a718809d59a69b3
tomek1kk <tomek1kkgw@gmail.com>
2020-01-06 18:06:48 +0100

    gitignore work

---
+++ SupremeBot/Controllers/TimerController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SupremeBot.Controllers
+{
+    public class TimerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SupremeBot/Models/Time.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SupremeBot.Models
+{
+    public class Time
+    {
+        public int Hour { get; set; }
+        public int Minute { get; set; }
+        public int Second { get; set; }
+    }
+}
