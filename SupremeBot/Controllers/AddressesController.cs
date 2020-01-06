32f38ba731c11588c732c25bfd9b64e20815add8
Piotr Koskiewicz <piotr888k@gmail.com>
2020-01-06 20:40:28 +0100

    addresses

---
+++ SupremeBot/Controllers/AddressesController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SupremeBot.Data;
+using SupremeBot.Models;
+
+namespace SupremeBot.Controllers
+{
+    public class AddressesController : Controller
+    {
+        private readonly DataContext _context;
+
+        public AddressesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [Route("Addresses/Index")]
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var cards = _context.Addresses.ToList();
+
+            return View(cards);
+        }
+
+        public async Task<ActionResult> Create()
+        {
+            return View();
+        }
+
+        public async Task<ActionResult> CreateAddress(Address address)
+        {
+            var model = new Address() { };
+
+            _context.Addresses.Add(address);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public async Task<ActionResult> Delete(int? id)
+        {
+            _context.Addresses.Remove(new Address() { Id = id.Value });
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+
+    }
+}
