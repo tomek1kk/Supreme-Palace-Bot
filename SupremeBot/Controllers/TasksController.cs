3139e96a98d974a5be2bad14be9b5f107a511727
Piotr Koskiewicz (PIT) <PIT@KMDPoland.pl>
2020-01-07 16:21:20 +0100

    create task view

--- SupremeBot/Controllers/TasksController.cs
+++ SupremeBot/Controllers/TasksController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SupremeBot.Data;
+using SupremeBot.Models;
 
 namespace SupremeBot.Controllers
 {
@@ -22,6 +23,29 @@ namespace SupremeBot.Controllers
         {
             var items = _context.TaskItems.ToList();
             return View(items);
+        }
+
+        [Route("Tasks/Create1")]
+        [HttpGet]
+        public IActionResult Create1()
+        {
+            return View("Create1");
+        }
+
+        public IActionResult CreateTaskItem1(TaskItem task)
+        {
+            _context.TaskItems.Add(task);
+            _context.SaveChanges();
+
+            //return RedirectToAction("Create2");
+            return RedirectToAction("Index");
+        }
+
+        [Route("Tasks/Colors")]
+        [HttpGet]
+        public IEnumerable<Color> Colors()
+        {
+            return _context.Colors.ToList();
         }
     }
 }
