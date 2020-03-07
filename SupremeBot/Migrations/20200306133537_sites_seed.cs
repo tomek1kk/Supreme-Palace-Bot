d052ef70b7978ba81a403a43e6eef704c24f2778
tomek1kk <tomek1kkgw@gmail.com>
2020-03-07 10:55:25 +0100

    migracje

---
+++ SupremeBot/Migrations/20200306133537_sites_seed.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SupremeBot.Migrations
+{
+    public partial class sites_seed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Insert into sites values ('Supreme', 'https://supremenewyork.com/shop/all')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
